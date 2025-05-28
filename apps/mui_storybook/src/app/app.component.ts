2506d94f54b7d4abde99ab5c16278855503f92e3
Dmitriy Smirnov <dima-smirnov1@mail.ru>
2025-05-28 16:16:02 +0300

    Update TailwindCSS to v4.1.7 and add new utility classes in `tailwind.css`, adjust dependencies and paths, and remove unused file `theme-sizes.type.ts`.

--- apps/mui_storybook/src/app/app.component.ts
+++ apps/mui_storybook/src/app/app.component.ts
@@ -1,13 +1,9 @@
 import { Component } from '@angular/core';
-import { RouterModule } from '@angular/router';
-import { MuiButtonComponent } from '@diz1/mui-kit';
 
 @Component({
-  imports: [ RouterModule, MuiButtonComponent ],
+  imports: [],
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
-export class AppComponent {
-  title = 'mui_storybook';
-}
+export class AppComponent {}
